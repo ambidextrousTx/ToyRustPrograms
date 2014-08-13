3f89110d2805289ef83d17018ceded346f3239a9
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-12 23:39:01 -0500

    Add function to check for equality

--- guess.rs
+++ guess.rs
@@ -1,9 +1,17 @@
+use std::io;
 use std::rand::random;
 
-fn main() {
-    let random_value = random::<u8>() % 10u8;
-    let mut guess = 0u8;
+fn guessed_correctly(guess: uint, machine: uint) -> bool {
+    if guess == machine {
+        return true;
+    }
+    return false;
+}
 
+fn main() {
+    let random_value = random::<uint>() % 10u;
+    let mut guess = 0u;
     println!("{}", random_value);
+    println!("{}", guessed_correctly(guess, random_value));
 
 }
