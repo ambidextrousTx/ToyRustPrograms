e130f5a52719452f225fa02a38f60de046e6317e
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-15 23:32:03 -0500

    Use modules and public functions

--- guess.rs
+++ guess.rs
@@ -1,14 +1,16 @@
-use std::io;
 use std::rand::random;
 
-fn guessed_correctly(guess: uint, machine: uint) -> bool {
-    guess == machine
+mod guess {
+    pub fn guessed_correctly(guess: uint, machine: uint) -> bool {
+        guess == machine
+    }
 }
 
 fn main() {
     let random_value = random::<uint>() % 10u;
     let mut guess = 0u;
+    guess += 1;
     println!("{}", random_value);
-    println!("{}", guessed_correctly(guess, random_value));
+    println!("{}", guess::guessed_correctly(guess, random_value));
 
 }
