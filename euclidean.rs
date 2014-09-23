d96ec3eb37fefa676686148ee121ce9538515c74
ambidextrousTx <ravisinha@my.unt.edu>
2014-09-22 23:35:10 -0500

    Fix initialization bug

--- euclidean.rs
+++ euclidean.rs
@@ -8,16 +8,17 @@ struct Point {
 
 mod pointutils {
     pub fn euclidean(point_a: Point, point_b: Point) {
+        0.0
     
     }
 
 }
 
 fn main() {
-    let point_a: Point = { x: 0.3, y: 20.0 };
-    let point_b: Point = { x: 4.0, y: -0.03 };
+    let point_a: Point = Point { x: 0.3, y: 20.0 };
+    let point_b: Point = Point { x: 4.0, y: -0.03 };
 
-
-
+    let euclidean_distance = pointutils::euclidean(point_a, point_b);
+    println!("Found distance: ", euclidean_distance);
 
 }
