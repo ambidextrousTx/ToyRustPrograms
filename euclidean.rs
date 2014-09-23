8c3f08a192d5abd425f655899742a75bdc53e40a
ambidextrousTx <ravisinha@my.unt.edu>
2014-09-22 23:32:30 -0500

    Set a struct to represent points, set up boilerplate

---
+++ euclidean.rs
@@ -0,0 +1,23 @@
+// Using structs to represent 2D points
+// and calculating the Euclidean distance between them
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+mod pointutils {
+    pub fn euclidean(point_a: Point, point_b: Point) {
+    
+    }
+
+}
+
+fn main() {
+    let point_a: Point = { x: 0.3, y: 20.0 };
+    let point_b: Point = { x: 4.0, y: -0.03 };
+
+
+
+
+}
