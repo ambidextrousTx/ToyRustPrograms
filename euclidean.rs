b41b158bc4c0e3dbf1bdee2ff18e5f7336a3e3e2
ambidextrousTx <ravisinha@my.unt.edu>
2014-09-22 23:47:36 -0500

    Add pow and sqrt functions, return actual result

--- euclidean.rs
+++ euclidean.rs
@@ -2,6 +2,7 @@
 // and calculating the Euclidean distance between them
 
 mod pointutils {
+    use std::num::pow;
 
     pub struct Point {
         pub x: f64,
@@ -10,8 +11,8 @@ mod pointutils {
 
 
     pub fn euclidean(point_a: Point, point_b: Point) -> f64 {
-        0.0
-
+        let value_sq = pow((point_a.x - point_b.x), 2) + pow((point_a.y - point_b.y), 2);
+        value_sq.sqrt()
     }
 }
 
@@ -20,7 +21,6 @@ fn main() {
     let point_b: pointutils::Point = pointutils::Point { x: 4.0, y: -0.03 };
 
     let euclidean_distance = pointutils::euclidean(point_a, point_b);
-    // let euclidean_distance = euclidean(point_a, point_b);
     println!("Found distance: {}", euclidean_distance);
 
 }
